11139b2bbfb43908385cc8ed6ba8a2ea4fc605b4
Liam <liamjee017@gmail.com>
2025-02-19 10:49:39 +0100

    Add files via upload

---
+++ sklep/nav.php
@@ -0,0 +1,18 @@
+<div class="navbar">
+    <button id="toggle-btn">///</button>
+    <h3 class="navbar-title">Aparaty.pl</h3>
+    <div class="search-container">
+        <form id="search-form" action="index.php" method="GET">
+            <input type="text" name="query" placeholder="Szukaj..." class="search-input" id="search-input">
+        </form>
+    </div>
+</div>
+
+<div id="navbar_bottom" class="side-navbar">
+    <ul>
+        <li><a href="index.php">Strona Główna</a></li>
+        <li><a href="index.php?id=2">Aparaty</a></li>
+        <li><a href="index.php?id=3">Producenci</a></li>
+        <li><a href="index.php?id=4">Koszyk</a></li>
+    </ul>
+</div>
